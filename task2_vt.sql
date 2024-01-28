cc7eb1320fcb62f384184335c18457d883640614
Vladislava-Tonarova <tonarovv@seznam.cz>
2024-01-28 17:18:54 +0100

    formated

--- task2_vt.sql
+++ task2_vt.sql
@@ -15,11 +15,11 @@ WHERE food LIKE 'Mléko%' OR food LIKE 'Chléb%'
 
 
 SELECT 	a_year, 
-		round(AVG(avg_year_wage)) AS avg_year_wage, 
-		food, 
-		avg_year_price, 
-		price_unit,
-		round(AVG(avg_year_wage) / avg_year_price) AS no_kg_l
+	round(AVG(avg_year_wage)) AS avg_year_wage, 
+	food, 
+	avg_year_price, 
+	price_unit,
+	round(AVG(avg_year_wage) / avg_year_price) AS no_kg_l
 FROM t_vladislava_tonarova_project_sql_primary_final tvt
 WHERE (food LIKE 'Mléko%' OR food LIKE 'Chléb%') AND (a_year = '2006' OR a_year = '2018')
 GROUP BY a_year, food ;
